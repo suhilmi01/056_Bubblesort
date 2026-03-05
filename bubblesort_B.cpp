006953c16a3e1f5dcd9267dde75a748747ee761e
suhilmi01 <suhilmi01@mail.com>
2026-03-05 18:34:08 +0700

    sort array

--- bubblesort_B.cpp
+++ bubblesort_B.cpp
@@ -30,3 +30,30 @@ void input()
         cin >> arr[i]; 
     }
 }
+void bubblesortarray()
+{
+    int pass = 1;
+
+    do
+    {
+        for (int j = 0; j <= n - 1 pass; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass += 1;
+
+        count << "\npass " << pass - 1 << ": ";
+        for (ink k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+        cout << endl;
+
+    } while (pass <= n - 1 );
+}
